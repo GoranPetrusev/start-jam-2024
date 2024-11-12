b69b7356f0e63c2987cfe8613163f85e87c5689b
GoranPetrusev <goranp01@outlook.com>
2024-11-12 20:44:24 +0100

    Refactor movement script and add testing area

--- player.gd
+++ player.gd
@@ -1,25 +1,37 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED := 300.0
+const JUMP_VELOCITY := -500.0
+const MAX_FALLING_SPEED := 900.0
+const FALLING_MULTIPLIER := 2.25
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+
 	if not is_on_floor():
+		fall(delta)
+		
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		
+	if Input.is_action_just_released("jump") and velocity.y < -100.0:
+		velocity.y = -100.0
+
+	process_movement()
+
+	move_and_slide()
+	
+
+func fall(delta:float) -> void:
+	if(velocity.y > 0.0):
+		velocity += get_gravity() * FALLING_MULTIPLIER * delta
+	else:
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+func process_movement() -> void:
+	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
